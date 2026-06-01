ad766d75f4f41ebcbfbc2518e97a31d8ac8f0753
huythoren12 <huyle@sheridancollege.ca>
2026-06-01 12:58:14 -0400

    Add Calculator class with divide method and main

--- src/main/java/com/mycompany/projecthuyle/ProjectHuyLe.java
+++ src/main/java/com/mycompany/projecthuyle/ProjectHuyLe.java
@@ -9,6 +9,12 @@ package com.mycompany.projecthuyle;
  * @author HP
  */
 public class ProjectHuyLe {
+      public double divide(double a, double b) {
+    if (b == 0) {
+        throw new ArithmeticException("Cannot divide by zero");
+    }
+    return a / b;
+}
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
