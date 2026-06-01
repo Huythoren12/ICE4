f669a31f27c9bc43a60bdb19f77c9b42c9a71525
huythoren12 <huyle@sheridancollege.ca>
2026-06-01 13:19:17 -0400

    Add addition method to Calculator

--- src/main/java/com/mycompany/projecthuyle/ProjectHuyLe.java
+++ src/main/java/com/mycompany/projecthuyle/ProjectHuyLe.java
@@ -16,6 +16,10 @@ public class ProjectHuyLe {
     return a / b;
 }
 
+      public double add(double a, double b) {
+    return a + b;
+}
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
